a43ae09442e8615a2e8054a2743b35536a50833b
JPGonzalezT <juapabgonzalez@utp.edu.co>
2024-05-31 11:58:55 -0500

    mno

--- my_card/card/admin.py
+++ my_card/card/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Card
 
 class CardAdmin(admin.ModelAdmin):
-    fieldsets = [('Datos de la persona', {'fields': ['name', 'email', 'mensaje']})]
-    list_display = ('name', 'email', 'mensaje')
-    list_filter = ['name']
-    search_fields = ['name']
+    fieldsets = [('Datos de la persona', {'fields': ['nombres', 'apellidos', 'telefono','ciudad', 'email', 'mensaje']})]
+    list_display = ('nombres', 'apellidos', 'telefono','ciudad', 'email', 'mensaje')
+    list_filter = ['nombres']
+    search_fields = ['nombres']
 
 admin.site.register(Card, CardAdmin)
 # Register your models here.
--- my_card/card/models.py
+++ my_card/card/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 class Card(models.Model):
     # Campos del modelo
-    name = models.CharField(max_length=30)
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    telefono = models.EmailField()
+    ciudad = models.EmailField()
     email = models.EmailField()
     mensaje = models.CharField(max_length=800)
 
     def __str__(self):
-        return f"datos de la persona: {self.name}  {self.email} {self.mensaje}"
+        return f"datos de la persona: {self.nombres} {self.apellidos} {self.telefono} {self.ciudad} {self.email} {self.mensaje}"
